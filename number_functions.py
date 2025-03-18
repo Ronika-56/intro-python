1854e80766bdce16bec96d02733f1deb1c391523
Ronika-56 <vmburu56@gmail.com>
2025-03-18 03:23:32 -0400

    python basics

---
+++ number_functions.py
@@ -0,0 +1,8 @@
+import math
+
+x = 640000
+
+print(math.sqrt(x))
+print(math .pi)
+print(math .floor(78.88))
+print(math .ceil(65.15))
---
+++ strings.py
@@ -0,0 +1,11 @@
+message = "Hi , Hope U re Fine"
+print(message)
+# functions
+print(message.capitalize())
+print(message.upper())
+print(message.lower())
+print(message.islower())
+print(message.isalpha())
+print(message.count("re"))
+print(message.title())
+
